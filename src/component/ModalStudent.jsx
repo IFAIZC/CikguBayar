6c239d060c9685df7810d0572220f618afdde070
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-07-13 23:03:40 +0800

    x

--- src/component/ModalStudent.jsx
+++ src/component/ModalStudent.jsx
@@ -5,7 +5,7 @@ export default function ModalStudent() {
       <label htmlFor="my_modal_7" className="btn btn-success">Add Student</label>
 
       {/* Modal */}
-      {/* GOING TO INSERT DATA TO SUPABASE */}
+      {/* GOING TO INSERT DATA TO SUPABASE - 14/7/2025 */} 
       <input type="checkbox" id="my_modal_7" className="modal-toggle" />
       <div className="modal" role="dialog">
         <div className="modal-box" style={{ maxWidth: "300px" }}>
