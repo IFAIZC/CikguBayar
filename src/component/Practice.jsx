15b63b27474a5e4a3234976146608e8a79f99bb2
Irfan Faiz Choo <165414147+IFAIZC@users.noreply.github.com>
2025-08-29 23:20:32 +0800

    p uE

--- src/component/Practice.jsx
+++ src/component/Practice.jsx
@@ -2,61 +2,16 @@ import { useEffect, useState } from "react"
 import supabase from "../../supabaseClient"
 
 export default function Practice() {
-  // const [loading, setLoading] = useState(true)
-  const [carData, setCarData] = useState([])
-
-  useEffect(() => {
-    async function fetchCarData() {
-      const { data, error } = await supabase
-      .from("car")
-      .select("*")
-
-      if (error){
-        console.error("Error fetching data : ", error)
-      } else {
-        setCarData(data)
-        console.log(data)
-      }
-    }
-    fetchCarData()
-  }, []); // Empty dependency array = run once
-
-  function purchaseNotification(car) {
-    window.alert(`You have purchased ${car.brand}-${car.model}`)
-    return
-  }
-
-  useEffect(() => {
-    // call your api here
+  
+  useEffect(()=>{
     // logic here
-    // edge case
-  }, []);
-
-  // added delete function
-  async function deleteFunction(car) {
-    const { error } = await supabase
-      .from('car')
-      .delete()
-      .eq('id', car.id)
-
-      if (error) {
-        console.error("Error deleting this data : ", error)
-      } else {
-        window.location.href = '/dashboard'; //to redirect/refresh the page once submit.
-      }
-
-  }
+    
+    // return for clean up
+  },[]) //dependency if needed
 
   return(
     <div>
-      {carData.map((car,index)=>(
-        <div key={index} className="flex flex-row gap-1 mb-3 items-center justify-between max-w-80">
-          <h1>{car.brand}</h1>
-          <h1>{car.model}</h1>
-          <button onClick={() => purchaseNotification(car)} className="btn btn-success">Buy now</button>
-          <button className="btn btn-warning" onClick={() => deleteFunction(car)}>Delete</button>
-        </div>
-      ))}
+      <p>test</p>
     </div>
   )
 }
